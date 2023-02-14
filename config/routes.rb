c1518ae055b7a42932052335fa92a268d731bbd6
amon-cofie <ddd.amon123@gmail.com>
2023-02-14 15:21:37 +0000

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,16 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # get "/public_recipes", to: 'public_recipes#index'
+  # get "/public_recipes/:id", to: 'public_recipes#show'
+
+  resources :users, only: [:show]
+  resources :recipes, only: [:index, :show] do 
+    resources :recipe_foods, only: [:new, :create, :destroy]  
+  end
+  resources :foods, only: [:index, :show]
+  resources :shopping_lists, only: [:index, :show]
+
+  
 end
