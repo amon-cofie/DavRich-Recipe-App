a73e6951669b4060d8fc94eb6cd6b16ca60d5605
amon-cofie <ddd.amon123@gmail.com>
2023-02-14 15:22:30 +0000

    Add recipes controller

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,9 @@
+class RecipesController < ApplicationController
+    def index
+        @recipes = Recipe.all
+    end
+
+    def show
+        @recipe = Recipe.where(:id = params[:id])    
+    end
+end
