80a8753b35f30a45ad01f51b19d048515b05d89d
amon-cofie <ddd.amon123@gmail.com>
2023-02-14 15:22:18 +0000

    Add recipe foods controller

---
+++ app/controllers/recipe_foods_controller.rb
@@ -0,0 +1,17 @@
+class RecipeFoodsController < ApplicationController
+    def new
+        @recipe = Recipe.find(params[:recipe_id])
+        @recipe_new = @recipe.recipe_foods.new
+        @foods = Food.all
+    end
+
+    def create
+
+    end
+
+    def destroy
+        
+    end
+
+
+end
