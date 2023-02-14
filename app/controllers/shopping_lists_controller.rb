423a4d87896aa53ddea441c0802ffd8e5a776f84
amon-cofie <ddd.amon123@gmail.com>
2023-02-14 15:22:46 +0000

    Add shopping lists controller

---
+++ app/controllers/shopping_lists_controller.rb
@@ -0,0 +1,16 @@
+class ShoppingListsController < ApplicationController
+    def index
+        @all_ingredients = RecipeFood.includes(:recipes, :foods)
+        @missing = @all_ingredients.select {|ing| ing.quantity > ing.foods.quantity}
+        @missing_ingredients = missing.map do |ingredient|
+            {
+                name: ingredient.foods.name,
+                quantity: ingredient.quantity - ingredient.foods.quantity,
+                price: ingredient.foods.price * (ingredient.quantity - ingredient.foods.quantity)
+            }
+        end
+        @total_price = @missing_ingredients.map {|ing| ing[:price]}.sum
+
+    end
+
+end
