6b90937a9326f8ba1afa10ccf314404d521ffe38
RichardChileya <richardchileyawk@gmail.com>
2023-02-15 10:31:03 +0200

    Update routes with Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  #root "recipes#index"
 
   # get "/public_recipes", to: 'public_recipes#index'
   # get "/public_recipes/:id", to: 'public_recipes#show'
