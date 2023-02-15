9ed15a47aed50c0bcd6af55cc31eef90f4ed68c2
RichardChileya <richardchileyawk@gmail.com>
2023-02-15 23:58:11 +0200

    Update recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,36 @@ class RecipesController < ApplicationController
     @recipes = Recipe.all
   end
 
-  def show
-    @recipe = Recipe.where(id: params[:id])
+
+
+  def new
+    @recipe = Recipe.new
   end
-end
+
+  def create 
+    @recipe = Recipe.new(name: params[:name], preparation_time: params[:preparation_time], cooking_time: params[:cooking_time], description: params[:description], public: params[:public])
+    @recipe.user = current_user
+    if @recipe.save
+      redirect_to_recipes_path, notice = 'recipe added successfully'
+    else
+      render new, alert = 'sorry an error happened. try again'
+    end
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = RecipeFood.includes(:food)
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to_recipes_path, alert = 'Recipe has been deleted'
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, description, :public)
+  end
+end 
