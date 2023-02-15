903e7dc213bf7f608fcc30df22e2bb0d61abd201
RichardChileya <richardchileyawk@gmail.com>
2023-02-15 10:30:33 +0200

    Update devise environment rb file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  #setup the host port 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
   # Enable server timing
   config.server_timing = true
 
