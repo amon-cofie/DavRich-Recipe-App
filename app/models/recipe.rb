3a618bae018259f1effadc55da711a957f35dd0d
RichardChileya <richardchileyawk@gmail.com>
2023-02-16 20:50:23 +0200

    fix recipe space linters error

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,7 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods, dependent: :destroy
-
+  
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
