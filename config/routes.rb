0ad3bda68f8e2b72573a7a02068baba0d73b24eb
amon-cofie <ddd.amon123@gmail.com>
2023-02-16 09:28:08 +0000

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   # get "/public_recipes", to: 'public_recipes#index'
   # get "/public_recipes/:id", to: 'public_recipes#show'
 
-  resources :users, only: [:show]
+  
   resources :recipes, only: [:index, :show] do 
     resources :recipe_foods, only: [:new, :create, :destroy]  
   end
-  resources :foods, only: [:index, :show]
+  resources :foods, only: [:index, :new, :show, :create]
   resources :shopping_lists, only: [:index, :show]
-
+  
   
 end
