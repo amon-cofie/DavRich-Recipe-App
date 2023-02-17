ae306945d7064a4524cf35f79feac82dea6374ca
RichardChileya <richardchileyawk@gmail.com>
2023-02-18 00:46:04 +0200

    Update recipe_foods controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -5,7 +5,24 @@ class RecipeFoodsController < ApplicationController
     @foods = Food.all
   end
 
-  def create; end
+  def create
+    @recipe_foods = RecipeFood.new(recipe_foods_params)
+    if @recipe_foods.save
+      redirect_to recipe_path(params[:recipe_id])
+    else
+      render :new
+    end
+  end
 
-  def destroy; end
+  def destroy
+    @recipe_food = RecipeFood.find(params[:id])
+    @recipe_food.destroy
+    redirect_to recipe_path(params[:recipe_id])
+  end
+
+  private
+
+  def recipe_foods_params
+    params.require(:recipe_food).permit(:quantity, :food_id, :recipe_id)
+  end  
 end
