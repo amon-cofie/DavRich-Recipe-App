2d06ae5199d3b403a47c43f7078693cc3f1907b7
RichardChileya <richardchileyawk@gmail.com>
2023-02-17 14:42:39 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,10 @@ Rails.application.routes.draw do
 
   # get "/public_recipes", to: 'public_recipes#index'
   # get "/public_recipes/:id", to: 'public_recipes#show'
-
-  resources :users, only: [:show]
+  
   resources :recipes, only: [:index, :new, :show, :create] do 
     resources :recipe_foods, only: [:new, :create, :destroy]  
   end
-  resources :foods, only: [:index, :show]
+  resources :foods, only: [:index, :new, :show, :create]
   resources :shopping_lists, only: [:index, :show]
 end
