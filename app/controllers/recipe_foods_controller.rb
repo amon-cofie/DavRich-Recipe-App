02ac8bbc7e24974cdce7343ceb5156bf67f3a3e7
amon-cofie <ddd.amon123@gmail.com>
2023-02-17 18:49:49 +0000

    Add user authentication to recipe_foods controller

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,4 +1,5 @@
 class RecipeFoodsController < ApplicationController
+  before_action :authenticate_user!
   def new
     @recipe = Recipe.find(params[:recipe_id])
     @recipe_new = @recipe.recipe_foods.new
