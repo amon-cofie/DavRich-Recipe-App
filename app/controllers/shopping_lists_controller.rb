ed919dded0cf5dffcd3a9354dc8cd8cc826c1f1c
RichardChileya <richardchileyawk@gmail.com>
2023-02-18 18:03:13 +0200

    fix buttpn error

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -2,12 +2,12 @@ class ShoppingListsController < ApplicationController
   before_action :authenticate_user!
   def index
     @all_ingredients = RecipeFood.includes(:recipe, :food)
-    @missing = @all_ingredients.select { |ing| ing.quantity > ing.foods.quantity }
+    @missing = @all_ingredients.select { |ing| ing.quantity > ing.food.quantity }
     @missing_ingredients = @missing.map do |ingredient|
       {
-        name: ingredient.foods.name,
-        quantity: ingredient.quantity - ingredient.foods.quantity,
-        price: ingredient.foods.price * (ingredient.quantity - ingredient.foods.quantity)
+        name: ingredient.food.name,
+        quantity: ingredient.quantity - ingredient.food.quantity,
+        price: ingredient.food.price * (ingredient.quantity - ingredient.food.quantity)
       }
     end
     @total_price = @missing_ingredients.map { |ing| ing[:price] }.sum
